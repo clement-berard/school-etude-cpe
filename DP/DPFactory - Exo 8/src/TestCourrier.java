55a31283ca02526d5e91d5c5e6c6ae4bae1c2f8c
keload <keload@gmail.com>
2014-02-20 15:07:12 +0100

    ds

--- DP/DPFactory - Exo 8/src/TestCourrier.java
+++ DP/DPFactory - Exo 8/src/TestCourrier.java
@@ -1,74 +1,67 @@
-interface Contenu
-{
+interface Contenu {
 	void encode(String texte);
+
 	String toString();
 }
-class ContenuTexte implements Contenu
-{
+
+class ContenuTexte implements Contenu {
 	protected String texte;
-	public void encode(String texte)
-	{
+
+	public void encode(String texte) {
 		this.texte = texte;
 	}
-	public String toString()
-	{
+
+	public String toString() {
 		return (texte);
 	}
 }
-class ContenuHtml implements Contenu
-{
+
+class ContenuHtml implements Contenu {
 	protected String codeHtml;
-	public void encode(String texte)
-	{
+
+	public void encode(String texte) {
 		codeHtml = "<HTML>" + texte + "</HTML>";
 	}
-	public String toString()
-	{
+
+	public String toString() {
 		return (codeHtml);
 	}
 }
 
-abstract class Courrier
-{
+abstract class Courrier {
 	protected Contenu contenu;
 	protected String destinataire;
 
-	abstract protected Contenu nouveauContenu();	// Factory Method
+	abstract protected Contenu nouveauContenu(); // Factory Method
 
-	public void prepare(String destinataire, String texte)
-	{
+	public void prepare(String destinataire, String texte) {
 		this.destinataire = destinataire;
 		contenu = nouveauContenu();
 		contenu.encode(texte);
 	}
-	public String toString()
-	{
+
+	public String toString() {
 		String st = "destinataire : " + destinataire + "\n";
 		st += "contenu : " + contenu.toString();
 		return st;
 	}
 }
 
-class CourrierTexte extends Courrier
-{
-	protected Contenu nouveauContenu()
-	{
+class CourrierTexte extends Courrier {
+	protected Contenu nouveauContenu() {
 		return new ContenuTexte();
 	}
 }
-class CourrierHtml extends Courrier
-{
-	protected Contenu nouveauContenu()
-	{
+
+class CourrierHtml extends Courrier {
+	protected Contenu nouveauContenu() {
 		return new ContenuHtml();
 	}
 }
 
-public class TestCourrier
-{
+public class TestCourrier {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		CourrierHtml courrierHtml = new CourrierHtml();
 		courrierHtml.prepare("adresse1@domaine", "texte1");
 		System.out.println(courrierHtml);
