4eb2d7105743429184b2582af7eaef382c751e87
keload <keload@gmail.com>
2014-02-20 16:49:25 +0100

    fds

--- DP/DPFactory - Exo 8/src/Courrier.java
+++ DP/DPFactory - Exo 8/src/Courrier.java
@@ -1,15 +1,32 @@
-abstract class Courrier {
+class Courrier {
+
 	protected Contenu contenu;
 	protected String destinataire;
 
-	abstract protected Contenu nouveauContenu(); // Factory Method
+	public Courrier(String type) {
+		
+	}
 
+	/**
+	 * 
+	 * @return
+	 */
+	//abstract protected Contenu nouveauContenu(); // Factory Method
+
+	/**
+	 * 
+	 * @param destinataire
+	 * @param texte
+	 */
 	public void prepare(String destinataire, String texte) {
 		this.destinataire = destinataire;
 		contenu = nouveauContenu();
 		contenu.encode(texte);
 	}
 
+	/**
+	 * 
+	 */
 	public String toString() {
 		String st = "destinataire : " + destinataire + "\n";
 		st += "contenu : " + contenu.toString();
