07b143282944b1a0b911ade28ac2450c9c79760c
keload <keload@gmail.com>
2014-03-25 09:36:57 +0100

    up

---
+++ DP/DP FichiersPourExemples/marsupiaux/TreeSleeperAdapterMammouth.java
@@ -0,0 +1,62 @@
+public class TreeSleeperAdapterMammouth implements ITreeSleeper {
+
+	public TreeSleeperAdapterMammouth(IElephantidae e) {
+		super();
+
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sleepInTree() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void awakening() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getSleepLong() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSleepShort() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean getSleepState() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setSleepLib() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setAwakeLib() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DP/DP FichiersPourExemples/marsupiaux/TreeSleeperDecorateObservable.java
@@ -0,0 +1,25 @@
+import java.util.Observable;
+import java.util.Observer;
+
+
+public class TreeSleeperDecorateObservable implements IObservable {
+
+	@Override
+	public void addObserver(Observer observateur) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void notifyObservers(Object o) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setObservable(Observable obs) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
